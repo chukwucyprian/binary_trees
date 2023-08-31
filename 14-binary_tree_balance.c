107d4961825696506138616ffc74ca7105056df1
MduduziSB <sibusisom.skhumkane@gmail.com>
2023-08-31 14:14:15 -0700

    Task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -10,7 +10,7 @@ int max(int a, int b)
 	return ((a > b) ? a : b);
 }
 /**
- * binary_tree_height - measures height of a binary tree
+ * tree_height - measures height of a binary tree
  * @tree: binary tree pointer to root node
  * Return: Height of binary tree
  */
@@ -31,14 +31,8 @@ int tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
-
 	if (tree == NULL)
 		return (0);
-
-	left_height = tree_height(tree->left);
-	right_height = tree_height(tree->right);
-
-	return (left_height - right_height);
+	return (tree_height(tree->left) - tree_height(tree->right));
 }
 
